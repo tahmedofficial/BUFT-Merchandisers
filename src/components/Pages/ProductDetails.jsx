861e1a6ad468985656216f5c5f3be17e838ab8bc
tahmedofficial <tanvirofcuo@gmail.com>
2025-01-17 21:20:49 +0600

    update product details

--- src/components/Pages/ProductDetails.jsx
+++ src/components/Pages/ProductDetails.jsx
@@ -9,12 +9,18 @@ const ProductDetails = ({ product }) => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-2'>
             <div className='md:p-20'>
-                <Image width="auto" height="auto" src={img} alt={slug}></Image>
+                <Image className='rounded-lg' width="auto" height="auto" src={img} alt={slug}></Image>
             </div>
             <div className='py-10 md:py-20'>
                 <div className='space-y-3'>
                     <h1 className='text-2xl md:text-3xl lg:text-4xl'>{title}</h1>
                     <h3>{description}</h3>
+                    <div className='space-y-2'>
+                        <h3>Minimum order 10000</h3>
+                        <h3>GSM: 150</h3>
+                        <h3>Fabric: Knit</h3>
+                        <h3>Button: 5</h3>
+                    </div>
                 </div>
             </div>
         </div>
